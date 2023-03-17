8bc88537dc40522c1aaf394199b8debf3b9bd31d
Ryan Wise <ryan.jeffwise@gmail.com>
2023-03-17 13:55:18 +1100

    Add language to Hello()

--- hello_test.go
+++ hello_test.go
@@ -4,15 +4,25 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("User")
+		got := Hello("User", "")
 		want := "Hello, User"
 
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
 }
