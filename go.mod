5fd7b67e8a8fe2378d871a35366e65eee188826b
Ryan Wise <ryan.jeffwise@gmail.com>
2023-03-17 13:24:45 +1100

    Hello World

---
+++ go.mod
@@ -0,0 +1,3 @@
+module learn-go-with-tests/m
+
+go 1.20
---
+++ hello_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("User")
+		want := "Hello, User"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// Let the test suite know this is a help[er function and defer the error location to the calling line
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
